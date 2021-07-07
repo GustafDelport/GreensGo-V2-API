abc1a45fc85a5c9c96f9a339f2c572a5dd08f8f8
Gustaf Delport <gustafdelport@gmail.com>
2021-07-07 12:44:41 +0200

    uHmm

--- routes/dataPoints.js
+++ routes/dataPoints.js
@@ -14,8 +14,28 @@ router.get('/all',async (req,res) => {
     }
 })
 
-router.post('/point',async (req,res) => {
-    console.log(req);
+//No use yet
+router.post('/send',async (req,res) => {
+     
+    const Data = new SensorData({
+         Time: req.body.Time,
+         Temperature: req.body.Temperature, 
+         HumidityPercentage: req.body.HumidityPercentage, 
+         MoisturePercentage: req.body.MoisturePercentage, 
+         LightIndex: req.body.LightIndex    
+    })
+
+    try {
+         const newData = await Data.save()
+         res.status(201).json(newData)
+
+    } catch (error) {
+         res.status(400).json({ message: error.message})
+    }
 })
+
+// router.post('/point',async (req,res) => {
+//     console.log(req);
+// })
 
 module.exports = router
